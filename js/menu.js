f74c711385b56f3f4fb5793f0421c9db1ab4d2c8
Renata-Silvax <renata.silva124@aluno.ce.gov.br>
2025-04-18 18:42:52 -0300

    Ajustando algumas paginas e testando paleta

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,42 @@
-//selecionar os itens clicados
-let menuItem = document.querySelectorAll('.item-menu')
+// //selecionar os itens clicados
+// let menuItem = document.querySelectorAll('.item-menu')
 
-function selectink() {
-    menuItem.forEach((item) =>
-        item.classList.remove('ativo')
-    )
-    this.classList.add('ativo')
+// function selectink() {
+//     menuItem.forEach((item) =>
+//         item.classList.remove('ativo')
+//     )
+//     this.classList.add('ativo')
+// }
+
+// menuItem.forEach((item) =>
+//     item.addEventListener('click', selectink)
+// )
+
+// //expandir o menu
+
+// let btnExp = document.querySelector('#btn-exp')
+// let menuSide = document.querySelector('.menu-lateral')
+
+// btnExp.addEventListener('click', function () {
+//     menuSide.classList.toggle('expadir')
+// })
+
+// Selecionar os itens clicados
+// Selecionar os itens clicados no menu
+let menuItem = document.querySelectorAll('.item-menu');
+
+function selectLink() {
+    menuItem.forEach((item) => item.classList.remove('ativo'));
+    this.classList.add('ativo');
 }
 
-menuItem.forEach((item) =>
-    item.addEventListener('click', selectink)
-)
+menuItem.forEach((item) => item.addEventListener('click', selectLink));
 
-//expandir o menu
-
-let btnExp = document.querySelector('#btn-exp')
-let menuSide = document.querySelector('.menu-lateral')
+// Expandir o menu lateral
+let btnExp = document.querySelector('#btn-exp');
+let menuSide = document.querySelector('.menu-lateral');
 
 btnExp.addEventListener('click', function () {
-    menuSide.classList.toggle('expadir')
-})
+    menuSide.classList.toggle('expandir');
+});
+
